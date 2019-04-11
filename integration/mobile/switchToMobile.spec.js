ae0e584b90887d785d1adfc72e0e7a43520238e0
anthropologiae <anthropologiae@gmail.com>
2019-04-11 16:03:00 +0300

    modified:   cypress.json 	modified:   cypress/fixtures/example.json 	deleted:    cypress/integration/desktop/authorization.spec.js 	deleted:    cypress/integration/desktop/registration.spec.js 	deleted:    cypress/integration/mobile/switchToMobile.spec.js 	modified:   cypress/support/commands.js 	modified:   cypress/support/index.js

--- integration/mobile/switchToMobile.spec.js
+++
@@ -1,7 +0,0 @@
-describe('Switch to mobile version', () => {
-    it('switch to mobile view', () => {
-
-        cy.visit(Cypress.config('baseUrl'));
-        cy.get('.mobile__text').click();
-    });
-});
