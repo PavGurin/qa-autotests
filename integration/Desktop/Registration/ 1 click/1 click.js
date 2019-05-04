f364aa91c25d0c8c746de5fd477bf626a94fb5b0
Alex <alex-top@1win.xyz>
2019-05-05 00:48:27 +0300

    - Add registration test

---
+++ integration/Desktop/Registration/ 1 click/1 click.js
@@ -0,0 +1,39 @@
+it('C16283', () => {
+    cy.visit('/');
+
+    cy
+        .get('header .level-right button.green')
+        .click();
+
+    cy
+        .get('.modal-container .form')
+        .last()
+        .as('register-form');
+
+    cy
+        .get('@register-form')
+        .find('.country-select input')
+        .should('have.value', 'Russia (Россия)');
+
+    cy
+        .get('@register-form')
+        .find('input[type="checkbox"]')
+        .check();
+
+    cy
+        .get('@register-form')
+        .find('.button-container button')
+        .click();
+
+    cy
+        .get('.modal-container .user-info .user-info__content__item')
+        .then(($el) => {
+            const login = $el[0].lastChild.outerText;
+            const password = $el[1].lastChild.outerText;
+
+            expect(login).not.to.be.empty;
+            expect(password).not.to.be.empty;
+
+            cy.log(`Логин - ${login}, Пароль - ${password}`);
+        });
+});
