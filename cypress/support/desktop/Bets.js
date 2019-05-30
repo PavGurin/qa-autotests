5ec580e0bf714a5dbe231b460a9e4b8187eb069a
niki4 <niki4@1win.pro>
2019-05-30 14:48:24 +0300

    fix bets test

--- cypress/support/desktop/Bets.js
+++ cypress/support/desktop/Bets.js
@@ -6,15 +6,20 @@ const make_bet_button = 'button.make-bet';
 const bet_amount_input = 'input[type=number]';
 const bet_is_done = 'div.coupon-status.success';
 const bet_not_done = 'div.coupon-status.error';
-const available_for_bet_element = '(//td[contains(@class,\'coupon-element\')][not(contains(@class,\'disabled\'))]' +
-    '/span[@class=\'value\'])[1]';
+const available_for_bet_element = () => cy
+    .get('.matches-block-content')
+    .find('.odd-cell')
+    .not('.disabled')
+    .find('.odd-coefficient')
+    .first();
 
 export const bets = {
 
     // делает ставку с главной страницы
     bet_main_page() {
         // выбирает первый доступный для ставки элемент
-        cy.xpath(available_for_bet_element)
+        // cy.xpath(available_for_bet_element)
+        available_for_bet_element()
           .click();
         // выделяет окно ввода суммы ставки, очищает данное поле и вводит '1'
         cy.get(bet_amount_input)
@@ -34,7 +39,7 @@ export const bets = {
         cy.get('div.sport-title')
           .should('be.visible');
         // выбирает первый доступный для ставки элемент
-        cy.xpath(available_for_bet_element)
+        available_for_bet_element()
           .click();
         // выделяет окно ввода суммы ставки, очищает данное поле и вводит '1'
         cy.get('input[type=number]')
@@ -51,7 +56,7 @@ export const bets = {
     // делает ставку с нулевой суммой
     bet_live_zero() {
         // выбирает первый доступный для ставки элемент
-        cy.xpath(available_for_bet_element)
+        available_for_bet_element()
           .trigger('mouseover')
           .click();
         // выделяет окно ввода суммы ставки, очищает данное поле и вводит '0'
