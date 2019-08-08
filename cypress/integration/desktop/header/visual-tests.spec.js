80d730fb395d3b6d72f4cea12cdd230672fd7919
Aleks2653 <outdead186@gmail.com>
2019-08-08 12:13:16 +0300

    add test  for screenshoot

---
+++ cypress/integration/desktop/header/visual-tests.spec.js
@@ -0,0 +1,23 @@
+import {auth} from "@support/desktop/Authorization";
+
+describe('Visual regression tests - эталонные фото', () => {
+    it('главная страница хэдер', () => {
+        cy.wait(5000);
+        cy.get('#header')
+        cy.matchImageSnapshot();
+    });
+    it('главная страница хэдер-зареганный пользователь', () => {
+        auth.login();
+        cy.wait(3000);
+        cy.get('#header')
+            .matchImageSnapshot();
+    });
+    it('кейсы', () => {
+        cy.get(':nth-child(5) > .item-text-block > .item-text')
+            .trigger("mouseover");
+            //.click();
+        cy.wait(3000);
+        cy.get('#main-container > div.content-wrapper > div > div > div.main-content')
+        .matchImageSnapshot();
+    });
+});
