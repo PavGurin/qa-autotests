5da3a9c77fe291ab895ec378b082650cd9ae8a49
pavgurin <kalliak1993@yandex.ru>
2019-08-08 15:19:16 +0300

    fixed cy.matchImageSnapshot() function

--- cypress/integration/desktop/header/visual-tests.spec.js
+++ cypress/integration/desktop/header/visual-tests.spec.js
@@ -1,16 +1,17 @@
 import {auth} from "@support/desktop/Authorization";
 
+
 describe('Visual regression tests - эталонные фото', () => {
     it('главная страница хэдер', () => {
         cy.wait(5000);
-        cy.get('#header')
+        cy.get('#header');
         cy.matchImageSnapshot();
     });
     it('главная страница хэдер-зареганный пользователь', () => {
         auth.login();
         cy.wait(3000);
-        cy.get('#header')
-            .matchImageSnapshot();
+        cy.get('#header');
+        cy.matchImageSnapshot();
     });
     it('кейсы', () => {
         cy.get(':nth-child(5) > .item-text-block > .item-text')
@@ -18,6 +19,6 @@ describe('Visual regression tests - эталонные фото', () => {
             //.click();
         cy.wait(3000);
         cy.get('#main-container > div.content-wrapper > div > div > div.main-content')
-        .matchImageSnapshot();
+        cy.matchImageSnapshot();
     });
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,17 +1,17 @@
+const webpack = require('@cypress/webpack-preprocessor');
 const {
   addMatchImageSnapshotPlugin,
 } = require('cypress-image-snapshot/plugin');
+
 module.exports = (on, config) => {
-  addMatchImageSnapshotPlugin(on, config);
-};
-
-const webpack = require('@cypress/webpack-preprocessor')
-
-module.exports = (on) => {
   const options = {
     webpackOptions: require('../../webpack.config.js'),
     watchOptions: {},
-  }
+  };
+
+  addMatchImageSnapshotPlugin(on, config);
 
   on('file:preprocessor', webpack(options))
-}
+};
+
+
