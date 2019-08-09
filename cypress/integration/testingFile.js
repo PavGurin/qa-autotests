d419efb5d9ab759f0eb567cd160120cd9df857a9
pavgurin <kalliak1993@yandex.ru>
2019-08-09 16:15:17 +0300

    first run

--- cypress/integration/testingFile.js
+++ cypress/integration/testingFile.js
@@ -1,6 +1,6 @@
 
 describe('The Home Page', function() {
     it('successfully loads', function() {
-        cy.visit('') // change URL to match your dev URL
+        cy.log("Hello world")// change URL to match your dev URL
     })
 });
