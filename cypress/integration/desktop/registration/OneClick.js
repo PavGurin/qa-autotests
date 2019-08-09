0bb0096c5281968d68b3391eb7a42a03e6147fca
pavgurin <kalliak1993@yandex.ru>
2019-08-09 12:25:08 +0300

    cleared project for runner-remake

--- cypress/integration/desktop/registration/OneClick.js
+++ cypress/integration/desktop/registration/OneClick.js
@@ -12,38 +12,4 @@ describe('Sign up in One click', () => {
         navReg.close_new_user_info();
         auth.logout();
     });
-
-    it('C16285 - One click sign up with Russia country with promo code', () => {
-        navReg.click_register();
-        navReg.accept_agreement();
-        navReg.add_promocode('test001');
-        navReg.sign_up();
-        // проверяет, что логин/пароль после регистрации не пустые и логирует их
-        navReg.check_reg_result();
-        navReg.close_new_user_info();
-        auth.logout();
-    });
-
-    it('C16284 - One click sign up with @country without promo code', () => {
-        navReg.click_register();
-        navReg.set_country("Andorra");
-        navReg.accept_agreement();
-        navReg.sign_up();
-        // проверяет, что логин/пароль после регистрации не пустые и логирует их
-        navReg.check_reg_result();
-        navReg.close_new_user_info();
-        auth.logout();
-    });
-
-    it('C16286 - One click sign up with @country with promo code', () => {
-        navReg.click_register();
-        navReg.set_country('Angola');
-        navReg.accept_agreement();
-        navReg.add_promocode('test001');
-        navReg.sign_up();
-        // проверяет, что логин/пароль после регистрации не пустые и логирует их
-        navReg.check_reg_result();
-        navReg.close_new_user_info();
-        auth.logout();
-    });
 });
