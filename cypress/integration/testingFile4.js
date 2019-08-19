5e28955f58af8b78a6fbf306e270cb5b2da8fe01
pavgurin <kalliak1993@yandex.ru>
2019-08-19 15:50:11 +0300

    added TR id

--- cypress/integration/testingFile4.js
+++ cypress/integration/testingFile4.js
@@ -1,6 +1,6 @@
 
 describe('The Home Page', function() {
-    it('successfully loads', function() {
+    it('C404327 successfully loads', function() {
         cy.log("Hello world");
         cy.wait(5000);
     })
