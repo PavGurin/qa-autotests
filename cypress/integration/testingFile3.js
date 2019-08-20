a3e11d5e0cd84276291b85ff71fc711f8c64dc67
pavgurin <kalliak1993@yandex.ru>
2019-08-20 17:08:02 +0300

    4 runners + 4 test cases

---
+++ cypress/integration/testingFile3.js
@@ -0,0 +1,7 @@
+
+describe('The Home Page', function () {
+  it('C404326 successfully loads', function () {
+    cy.log('Hello world')
+    cy.wait(5000)
+  })
+})
---
+++ cypress/integration/testingFile4.js
@@ -0,0 +1,7 @@
+
+describe('The Home Page', function () {
+  it('C404327 successfully loads', function () {
+    cy.log('Hello world')
+    cy.wait(5000)
+  })
+})
