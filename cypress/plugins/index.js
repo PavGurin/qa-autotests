6df89e31a9945fc2cd90f19e86d4dfe3283e52d7
pavgurin <kalliak1993@yandex.ru>
2019-08-20 18:32:22 +0300

    --disable-dev-shm-usage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -5,6 +5,14 @@ module.exports = (on) => {
     webpackOptions: require('../../webpack.config.js'),
     watchOptions: {},
   }
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') {
+      args.push('--disable-dev-shm-usage')
+      return args
+    }
 
+    return args
+  })
   on('file:preprocessor', webpack(options))
 }
+
