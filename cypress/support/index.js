d674eb663fc9a3835705dffd3b4ea8eb7a9aba7f
pavgurin <kalliak1993@yandex.ru>
2019-08-22 16:50:03 +0300

    fixed cookies

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,6 +1,6 @@
 beforeEach(function () {
   cy.log('I run before every test in every spec file!!!!!!')
-  cy.clearCookies()
-  // //     cy.setCookie('session-id', '', {domain: '1win-auth.com'});
+  cy.clearCookies();
+  cy.setCookie('session-id', '', {domain: '1win-auth.com'});
   cy.visit('')
 })
