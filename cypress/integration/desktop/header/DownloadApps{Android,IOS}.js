3f250d78fa8f2f6fdd695b5de5549b599cd365d2
pavgurin <kalliak1993@yandex.ru>
2019-08-22 16:18:28 +0300

    added skip for download tests

--- cypress/integration/desktop/header/DownloadApps{Android,IOS}.js
+++ cypress/integration/desktop/header/DownloadApps{Android,IOS}.js
@@ -1,9 +1,10 @@
 import { navReg } from '@support/desktop/NavReg';
 describe.skip('Download apps', () => {
 
-    it.skip('C16786 - Download android', () => {
+    it('C16786 - Download android', () => {
         navReg.check_android_download();
     });
+
     it('C18503 - Download IOS(modal window)', () => {
         navReg.click_ios_download();
         navReg.check_ios_download_window();
