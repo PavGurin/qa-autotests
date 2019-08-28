8818425d841934f4c66b4743dbd981013a397713
pavgurin <kalliak1993@yandex.ru>
2019-08-28 16:44:37 +0300

    download android unskipped download ios skipped

--- cypress/integration/desktop/header/DownloadApps.js
+++ cypress/integration/desktop/header/DownloadApps.js
@@ -1,10 +1,10 @@
 import { navReg } from '@support/desktop/NavReg';
 describe('Download apps', () => {
 
-    it.skip('C16786 - Download android', () => {
+    it('C16786 - Download android', () => {
         navReg.check_android_download();
     });
-    it('C18503 - Download IOS(modal window)', () => {
+    it.skip('C18503 - Download IOS(modal window)', () => {
         navReg.click_ios_download();
         navReg.check_ios_download_window();
         navReg.ios_modal_close();
