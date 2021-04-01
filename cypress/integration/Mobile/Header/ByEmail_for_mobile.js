e65a44b33e9759d277961d0d46a1a165b1fb15c6
tatyana <tatyana.basalaeva@1win.pro>
2021-04-01 17:53:45 +0300

    refactored mobile registration

--- cypress/integration/Mobile/Header/ByEmail_for_mobile.js
+++ cypress/integration/Mobile/Header/ByEmail_for_mobile.js
@@ -19,15 +19,10 @@ describe("Registration by email", () => {
             .toString(36)
             .slice(-6);
     navReg.click_register_for_mobile();
-    navReg.registration_form("Email");
   });
   it("Without promocode", () => {
-    cy.get(".reg-full .submit-btn").first().click();
-    // navReg.set_name_for_mobile(`test_${randomStr}`)
-    // navReg.set_date_of_birth_for_mobile('2000-12-31')
     navReg.set_email_register_for_mobile(`${randomStr}test@xyz.com`);
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile(`921${randomNum}`);
     navReg.sign_up_email_continue();
     navReg.check_sign_up_for_mobile("1win");
@@ -36,21 +31,19 @@ describe("Registration by email", () => {
   });
 
   it("With promocode", () => {
-    cy.get(".reg-full .submit-btn").first().click();
     navReg.add_promocode_by_email_for_mobile("test123");
     navReg.set_email_register_for_mobile(`${randomStr}test@xyz.com`);
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
+    //navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile(`921${randomNum}`);
     navReg.sign_up_email_continue();
     navReg.check_sign_up_for_mobile("1win");
     auth.logout_for_mobile2();
   });
   it("With another country", () => {
-    cy.get(".reg-full .submit-btn").first().click();
     navReg.set_email_register_for_mobile(`${randomStr}test@xyz.com`);
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
+    //navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile(`921${randomNum}`);
     navReg.set_country_for_mobile("Andorra");
     navReg.set_phone_numb_for_mobile(`312${randomNum}`);
@@ -59,10 +52,9 @@ describe("Registration by email", () => {
     auth.logout_for_mobile2();
   });
   it("Existing phone number", () => {
-    cy.get(".reg-full .submit-btn").first().click();
     navReg.set_email_register_for_mobile(`${randomStr}test@xyz.com`);
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
+    //navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile("9211111111}");
     navReg.sign_up_email_continue();
     auth.check_notification_bad_number_phone_for_mobile();
@@ -71,10 +63,9 @@ describe("Registration by email", () => {
     cy.get("header button.close").click();
   });
   it("Existing email ", () => {
-    cy.get(".reg-full .submit-btn").first().click();
     navReg.set_email_register_for_mobile("nogm75@1win.xyz");
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
+    //navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile(`911${randomNum}`);
     navReg.sign_up_email_continue();
     auth.check_notification_bad_mail_for_mobile();
@@ -91,7 +82,7 @@ describe("Registration by email", () => {
       .should("have.text", "$");
     navReg.set_email_register_for_mobile(`${randomStr}test@xyz.com`);
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
+    //navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile(`921${randomNum}`);
     navReg.sign_up_email_continue();
     cy.get(".balance-value")
@@ -109,7 +100,7 @@ describe("Registration by email", () => {
       .should("have.text", "€");
     navReg.set_email_register_for_mobile(`${randomStr}test@xyz.com`);
     navReg.set_pwd_for_mobile("111111");
-    navReg.repeat_pwd_for_mobile("111111");
+    //navReg.repeat_pwd_for_mobile("111111");
     navReg.set_phone_numb_for_mobile(`921${randomNum}`);
     navReg.sign_up_email_continue();
     cy.get(".balance-value")
